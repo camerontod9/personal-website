eb7928c29db2b406c57f5ac4eeb5efcd00283dc4
camerontod9 <camerontod9@gmail.com>
2023-12-10 14:16:52 -0500

    fixed css routing and implemented main page layout

--- personal-website/src/components/home.js
+++ personal-website/src/components/home.js
@@ -1,17 +1,19 @@
 import { React, useEffect } from "react";
 import { Link } from "react-router-dom";
+import headshot from '../content/unknown.png'
+import './css/home.css';
 
 function Home() {
     return(
         <div className="home-page">
             <header className="header">
+                <Link to="./works" className="works-link header-link">portfolio link here</Link>
                 <h1 className="title">Cameron Todd</h1>
-                <h3 className="subtitle">An emerging web & UI designer with a passion for informed and human-centered design.</h3>
-                <Link to="./works">portfolio link here</Link>
-                <Link to="./about">what i believe in link here</Link>
+                <Link to="./about" className="header-link about-link">what i believe in link here</Link>
             </header>
+            <h3 className="subtitle">A web developer & UI designer with a passion for informed and human-centered design.</h3>
             <div className="photobox">
-                <img></img>
+                <img src={headshot} alt="Me!"></img>
                 <p>text text</p>
             </div>
         </div>
